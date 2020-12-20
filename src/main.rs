d40b2a5457b62a29d85ea5c321138a52957c8eb8
Michael Hitchens <mdhitchens@gmail.com>
2020-12-20 07:55:23 -0500

    Refactor paint lambda into PaintEvent (works better with borrow rules)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sdl2::event::Event;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::render::Texture;
@@ -12,6 +13,8 @@ mod monster;
 mod rms;
 
 mod tests;
+
+struct PaintEvent {}
 
 fn apply_mask(image: &mut img::Image, mask_image: &img::Image) {
     image
@@ -59,6 +62,12 @@ fn main() {
     let monsters = monster::load_monsters("PYMON.DAT");
 
     let sdl_context = sdl2::init().unwrap();
+
+    let event_subsystem = sdl_context.event().unwrap();
+    event_subsystem
+        .register_custom_event::<PaintEvent>()
+        .unwrap();
+
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
         .window("Hello", 640, 480)
@@ -110,60 +119,70 @@ fn main() {
         .collect();
 
     let mut debug_room_index: usize = 0;
-    let paint = &mut || {
-        canvas.clear();
-        for y in 0..rms::ROOM_HEIGHT {
-            for x in 0..rms::ROOM_WIDTH {
-                let draw_rect = Rect::new(
-                    (x * img::IMAGE_DIMENSION) as i32,
-                    (y * img::IMAGE_DIMENSION) as i32,
-                    img::IMAGE_DIMENSION,
-                    img::IMAGE_DIMENSION,
-                );
-
-                let mut tile = rooms[debug_room_index].get_tile(x, y);
-                if tile > 0 {
-                    tile -= 1;
-                    canvas
-                        .copy(&tiles_atlas[tile as usize], None, draw_rect)
-                        .unwrap();
-                }
-
-                match rooms[debug_room_index].get_object_type(x, y) {
-                    rms::ObjectType::Monster => {
-                        let monster_id = rooms[debug_room_index].monster_id - 1;
-                        tile = monsters[monster_id as usize].gfx_id - 1;
-                        canvas
-                            .copy(&monsters_atlas[tile as usize], None, draw_rect)
-                            .unwrap();
-                    }
-                    rms::ObjectType::Object => {
-                        tile = rooms[debug_room_index].get_object(x, y);
-                        if tile == 0 {
-                            continue;
-                        }
-                        // TODO: For some reason, I made it so I don't need to -1 here... when I probably should since it's ambiguous whether 0 is the "no tile" sentinel or literally tile 0
-                        // tile -= 1;
-                        canvas
-                            .copy(&tiles_atlas[tile as usize], None, draw_rect)
-                            .unwrap();
-                    }
-                    _ => {}
-                }
-            }
-        }
-        canvas.present();
-
-        debug_room_index = (debug_room_index + 1) % rooms.len();
-    };
-    paint();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'mainloop: loop {
         for event in event_pump.wait_iter() {
             match event {
-                sdl2::event::Event::Quit { .. } => break 'mainloop,
-                _ => paint(),
+                Event::Quit { .. } => break 'mainloop,
+                Event::User { .. } => {
+                    // HACK! The Rust-SDL2 API wants me to do something like:
+                    // if event.is_user_event() {
+                    //     let paint_event = event.as_user_event_type::<PaintEvent>().unwrap();
+                    //     // do paint
+                    // }
+                    // But I know under the covers that it's just a User event, which works better
+                    // in this match statement.
+                    canvas.clear();
+                    for y in 0..rms::ROOM_HEIGHT {
+                        for x in 0..rms::ROOM_WIDTH {
+                            let draw_rect = Rect::new(
+                                (x * img::IMAGE_DIMENSION) as i32,
+                                (y * img::IMAGE_DIMENSION) as i32,
+                                img::IMAGE_DIMENSION,
+                                img::IMAGE_DIMENSION,
+                            );
+
+                            let mut tile = rooms[debug_room_index].get_tile(x, y);
+                            if tile > 0 {
+                                tile -= 1;
+                                canvas
+                                    .copy(&tiles_atlas[tile as usize], None, draw_rect)
+                                    .unwrap();
+                            }
+
+                            match rooms[debug_room_index].get_object_type(x, y) {
+                                rms::ObjectType::Monster => {
+                                    let monster_id = rooms[debug_room_index].monster_id - 1;
+                                    tile = monsters[monster_id as usize].gfx_id - 1;
+                                    canvas
+                                        .copy(&monsters_atlas[tile as usize], None, draw_rect)
+                                        .unwrap();
+                                }
+                                rms::ObjectType::Object => {
+                                    tile = rooms[debug_room_index].get_object(x, y);
+                                    if tile == 0 {
+                                        continue;
+                                    }
+                                    // TODO: For some reason, I made it so I don't need to -1 here...
+                                    // when I probably should since it's ambiguous whether 0 is the "no tile" sentinel or literally tile 0
+                                    // tile -= 1;
+                                    canvas
+                                        .copy(&tiles_atlas[tile as usize], None, draw_rect)
+                                        .unwrap();
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                    canvas.present();
+
+                    debug_room_index = (debug_room_index + 1) % rooms.len();
+                }
+                // Convert all other events into paint events (keep the screen fresh)
+                _ => {
+                    event_subsystem.push_custom_event(PaintEvent {}).unwrap();
+                }
             }
         }
     }
